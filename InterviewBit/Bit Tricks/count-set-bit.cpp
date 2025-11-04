95c3fa11d57c57fd7d585e3d39a4166c2d5ea24e
Rkp420 <rkp20442@gmail.com>
2025-11-04 14:52:05 +0530

    Solved Count Set Bits Till N

--- InterviewBit/Bit Tricks/count-set-bit.cpp
+++ InterviewBit/Bit Tricks/count-set-bit.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int Mod = 1e9 + 7;
 
+int largestPowerOfTwo(int A)
+{
+    int x = 0;
+    while ((1 << x) <= A)
+        x++;
+    return x - 1;
+}
+
+int solve(int A)
+{
+    if (A == 0)
+        return 0;
+
+    int x = largestPowerOfTwo(A);
+
+    long long bitTill = (1LL << (x - 1)) * x;
+
+    long long msb = A - (1 << x) + 1;
+
+    int rest = A - (1 << x);
+
+    long long ans = (bitTill % Mod + msb % Mod + solve(rest) % Mod) % Mod;
+
+    return ans;
+}
 
 int main()
 {
